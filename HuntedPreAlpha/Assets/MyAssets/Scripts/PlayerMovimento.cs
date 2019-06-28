8b1fcdf7412a24ab87eb7eec2a9aa63651ed2106
Rcurreli <ricurreli96@gmail.com>
2019-06-28 13:53:19 +0200

    ciao

---
+++ HuntedPreAlpha/Assets/MyAssets/Scripts/PlayerMovimento.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovimento : MonoBehaviour
+{
+    public float vel = 1.0f;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {     
+        transform.forward* Input.GetAxis("Verticale") * vel * Time.deltaTime);
+        transform.forward* Input.GetAxis("Orizzontale") * vel * Time.deltaTime);
+
+        float velocita = vel + (transform.rotation.ToEulerAngles().x * 10);
+
+        rb.velocity = transform.forward * velocita;
+    }
+}
