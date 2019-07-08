5ca752881e62a79479bc70d70216744aafd3b2a6
Rcurreli <ricurreli96@gmail.com>
2019-07-08 18:47:33 +0200

    8 luglio

--- HuntedPreAlpha/Assets/MyAssets/Scripts/PlayerMovimento.cs
+++ HuntedPreAlpha/Assets/MyAssets/Scripts/PlayerMovimento.cs
@@ -9,158 +9,193 @@ public class PlayerMovimento : MonoBehaviour
     public GameObject Omino;
     private Rigidbody rb;
     private bool controllo;
-    private Quaternion ominoDir;
-    private Vector3 raggioDir;
-    private int asse; 
-    private Vector3 miaPosizione1;
-    private Vector3 miaPosizione2;
-    private RaycastHit chivedo1;
-    private RaycastHit chivedo2;
-    private bool vedoqualcosa1;
-    private bool vedoqualcosa2;
-    private Vector3 miaPosizione3;
-    private Vector3 miaPosizione4;
-    private RaycastHit chivedo3;
-    private RaycastHit chivedo4;
-    private bool vedoqualcosa3;
-    private bool vedoqualcosa4;
+    private bool move;
+    private Vector3 moveDir;
+    private Vector3 raysDirBlu;
+    private int raysAsse; 
+    private Vector3 rayPosRossoR;
+    private Vector3 rayPosRossoL;
+    private RaycastHit rayHitRossoR;
+    private RaycastHit rayHitRossoL;
+    private bool rayFindRossoR;
+    private bool rayFindRossoL;
+    private Vector3 rayPosBluR;
+    private Vector3 rayPosBluL;
+    private RaycastHit rayHitBluR;
+    private RaycastHit rayHitBluL;
+    private bool rayFindBluR;
+    private bool rayFindBluL;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        ominoDir = Omino.transform.rotation;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {    
-        ominoDir = Omino.transform.rotation;
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            if(ominoDir == Quaternion.LookRotation(transform.forward)){
-                Movimento(transform.forward);
-            }
-            else if(ominoDir == Quaternion.LookRotation(- transform.forward)){
-                Movimento(transform.forward);
-                raggioDir = transform.forward;
+            if(Omino.transform.rotation == Quaternion.LookRotation(transform.forward) || Omino.transform.rotation == Quaternion.LookRotation(- transform.forward)){
+                moveDir = transform.forward;
+                move = true;
             }
             else{
-                asse = 0;
-                raggioDir = transform.forward;
+                raysAsse = 0;
+                raysDirBlu = transform.forward;
                 controllo = true;
             }
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            if(ominoDir == Quaternion.LookRotation(- transform.forward)){
-                Movimento(- transform.forward);
+            if(Omino.transform.rotation == Quaternion.LookRotation(- transform.forward) || Omino.transform.rotation == Quaternion.LookRotation(transform.forward)){
+                moveDir = - transform.forward;
+                move = true;
             }    
-            else if(ominoDir == Quaternion.LookRotation(transform.forward)){
-                Movimento(- transform.forward);
-                raggioDir = - transform.forward;
-            }
             else{
-                asse = 0;
-                raggioDir = - transform.forward;
+                raysAsse = 0;
+                raysDirBlu = - transform.forward;
                 controllo = true;
             }    
         }
 
         else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            if(ominoDir == Quaternion.LookRotation(- transform.right)){
-                Movimento(- transform.right);
+            if(Omino.transform.rotation == Quaternion.LookRotation(- transform.right) || Omino.transform.rotation == Quaternion.LookRotation(transform.right)){
+                moveDir = - transform.right;
+                move = true;
             }  
-            else if(ominoDir == Quaternion.LookRotation(transform.right)){
-                Movimento(- transform.right);
-                raggioDir = - transform.right;
-            }
             else{
-                asse = 1;
-                raggioDir = - transform.right;
+                raysAsse = 1;
+                raysDirBlu = - transform.right;
                 controllo = true;
             }      
         }
 
         else if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            if(ominoDir == Quaternion.LookRotation(transform.right)){
-                Movimento(transform.right);
+            if(Omino.transform.rotation == Quaternion.LookRotation(transform.right) || Omino.transform.rotation == Quaternion.LookRotation(- transform.right)){
+                moveDir = transform.right;
+                move = true;
             }
-            else if(ominoDir == Quaternion.LookRotation(- transform.right)){
-                Movimento(transform.right);
-                raggioDir = transform.right;
-            }  
             else{
-                asse = 1;
-                raggioDir = transform.right;
+                raysAsse = 1;
+                raysDirBlu = transform.right;
                 controllo = true;
             }  
         }
+        else move = false;
 
-        //Raycast svolta
+        //Raycast svolta in FixedUpdate
+
+        //Movimento in FixedUpdate
+
+        //Raycast davanti in FixedUpdate
+
+    }
+
+    void FixedUpdate() {
         if(controllo){
-            if(raggioDir.x != 0 || raggioDir.z != 0){
-                miaPosizione3 = transform.position;
-                miaPosizione3.y += 0.5f;
-                miaPosizione4 = transform.position;
-                miaPosizione4.y += 0.5f;
-                if(asse == 0){
-                    miaPosizione3.x += 0.49f;
-                    miaPosizione4.x -= 0.49f;
-                }
-                else if(asse == 1){
-                    miaPosizione3.z += 0.49f;
-                    miaPosizione4.z -= 0.49f;
-                }
-                vedoqualcosa3 = Physics.Raycast(miaPosizione3, raggioDir, out chivedo3, 0.6f);
-                vedoqualcosa4 = Physics.Raycast(miaPosizione4, raggioDir, out chivedo4, 0.6f);
-
-                if ((!vedoqualcosa3 || chivedo3.transform.tag != "Muro")&&(!vedoqualcosa4 || chivedo4.transform.tag != "Muro")) {
-                    Movimento(raggioDir);
-                    controllo = false;
-                } 
-                Debug.DrawRay(miaPosizione3, raggioDir, Color.blue);
-                Debug.DrawRay(miaPosizione4, raggioDir, Color.blue);
+            if(RaysTurn()){
+                moveDir = raysDirBlu;
+                move = true;
+                controllo = false;
             }
         }
 
-        //Raycast davanti
-        miaPosizione1 = transform.position;
-        miaPosizione1.y += 0.5f;
-        miaPosizione2 = transform.position;
-        miaPosizione2.y += 0.5f;
-        ominoDir = Omino.transform.rotation;
-        if(ominoDir == Quaternion.LookRotation(- transform.forward) || ominoDir == Quaternion.LookRotation(transform.forward)){
-            miaPosizione1.x += 0.49f;
-            miaPosizione2.x -= 0.49f;
+        if(move){
+            Movimento(moveDir);
+            move = false;
         }
-        else if(ominoDir == Quaternion.LookRotation(- transform.right) || ominoDir == Quaternion.LookRotation(transform.right)){
-            miaPosizione1.z += 0.49f;
-            miaPosizione2.z -= 0.49f;
+
+        if(RaysForward()){
+            move = false;
+            rb.velocity = new Vector3(0,0,0);
+            if(Omino.transform.rotation == Quaternion.LookRotation(transform.forward)){
+                int divisione = (int)(transform.position.z / 0.5f);
+                if((transform.position.z - (divisione * 0.5f))> 0.3f) divisione +=1;
+                else if((transform.position.z - (divisione * 0.5f))< -0.3f) divisione -=1;
+                transform.position = new Vector3(transform.position.x,transform.position.y,divisione * 0.5f);
+            }
+            else if(Omino.transform.rotation == Quaternion.LookRotation(- transform.forward)){
+                int divisione = (int)(transform.position.z / 0.5f);
+                if((transform.position.z - (divisione * 0.5f))> 0.3f) divisione +=1;
+                else if((transform.position.z - (divisione * 0.5f))< -0.3f) divisione -=1;
+                transform.position = new Vector3(transform.position.x,transform.position.y,divisione * 0.5f);                
+            }
+            
+            else if(Omino.transform.rotation == Quaternion.LookRotation(transform.right)){
+                int divisione = (int)(transform.position.x / 0.5f);
+                if((transform.position.x - (divisione * 0.5f))> 0.3f) divisione +=1;
+                else if((transform.position.x - (divisione * 0.5f))< -0.3f) divisione -=1;
+                transform.position = new Vector3(divisione * 0.5f,transform.position.y,transform.position.z);
+            }
+
+            else if(Omino.transform.rotation == Quaternion.LookRotation(- transform.right)){
+                int divisione = (int)(transform.position.x / 0.5f);
+                if((transform.position.x - (divisione * 0.5f))> 0.3f) divisione +=1;
+                else if((transform.position.x - (divisione * 0.5f))< -0.3f) divisione -=1;
+                transform.position = new Vector3(divisione * 0.5f,transform.position.y,transform.position.z);
+            }    
         }
-        vedoqualcosa1 = Physics.Raycast(miaPosizione1, Omino.transform.forward, out chivedo1, 0.49f);
-        vedoqualcosa2 = Physics.Raycast(miaPosizione2, Omino.transform.forward, out chivedo2, 0.49f);
-        if(vedoqualcosa1){
-            if(chivedo1.transform.tag == "Muro"){
-                rb.velocity*=0;
-                if(ominoDir == Quaternion.LookRotation(- transform.forward) || ominoDir == Quaternion.LookRotation(transform.forward)){
-                    int divisione = (int)(transform.position.z / 0.5f);
-                    transform.position = new Vector3(transform.position.x,transform.position.y,divisione * 0.5f);
-                }
-            } 
-        }
-        if(vedoqualcosa2){
-            if(chivedo2.transform.tag == "Muro"){
-                rb.velocity*=0;
-                if(ominoDir == Quaternion.LookRotation(- transform.right) || ominoDir == Quaternion.LookRotation(transform.right)){
-                    int divisione = (int)(transform.position.x / 0.5f);
-                    transform.position = new Vector3(divisione * 0.5f,transform.position.y,transform.position.z);
-                }
-            } 
-        }
-        Debug.DrawRay(miaPosizione1, Omino.transform.forward, Color.red);
-        Debug.DrawRay(miaPosizione2, Omino.transform.forward, Color.red);
     }
 
-    private void Movimento(Vector3 dir){
+    bool RaysTurn(){
+        rayPosBluR = transform.position;
+        rayPosBluR.y += 0.5f;
+        rayPosBluL = transform.position;
+        rayPosBluL.y += 0.5f;
+        if(raysAsse == 0){
+            rayPosBluR.x += 0.49f;
+            rayPosBluL.x -= 0.49f;
+        }
+        else if(raysAsse == 1){
+            rayPosBluR.z += 0.49f;
+            rayPosBluL.z -= 0.49f;
+        }
+        rayFindBluR = Physics.Raycast(rayPosBluR, raysDirBlu, out rayHitBluR, 0.6f);
+        rayFindBluL = Physics.Raycast(rayPosBluL, raysDirBlu, out rayHitBluL, 0.6f);
+        
+        Debug.DrawRay(rayPosBluR, raysDirBlu, Color.blue);
+        Debug.DrawRay(rayPosBluL, raysDirBlu, Color.blue);
+
+        if ((!rayFindBluR || rayHitBluR.transform.tag != "Muro")&&(!rayFindBluL || rayHitBluL.transform.tag != "Muro")) {
+            return true;
+        } 
+        return false;
+    }
+
+    bool RaysForward(){
+        rayPosRossoR = transform.position;
+        rayPosRossoR.y += 0.5f;
+        rayPosRossoL = transform.position;
+        rayPosRossoL.y += 0.5f;
+        if(Omino.transform.rotation == Quaternion.LookRotation(- transform.forward) || Omino.transform.rotation == Quaternion.LookRotation(transform.forward)){
+            rayPosRossoR.x += 0.47f;
+            rayPosRossoL.x -= 0.47f;
+        }
+        else if(Omino.transform.rotation == Quaternion.LookRotation(- transform.right) || Omino.transform.rotation == Quaternion.LookRotation(transform.right)){
+            rayPosRossoR.z += 0.47f;
+            rayPosRossoL.z -= 0.47f;
+        }
+        rayFindRossoR = Physics.Raycast(rayPosRossoR, Omino.transform.forward, out rayHitRossoR, 0.49f);
+        rayFindRossoL = Physics.Raycast(rayPosRossoL, Omino.transform.forward, out rayHitRossoL, 0.49f);
+
+        Debug.DrawRay(rayPosRossoR, Omino.transform.forward, Color.red);
+        Debug.DrawRay(rayPosRossoL, Omino.transform.forward, Color.red);
+
+        if(rayFindRossoR){
+            if(rayHitRossoR.transform.tag == "Muro"){
+                return true;
+            } 
+        }
+        else if(rayFindRossoL){
+            if(rayHitRossoL.transform.tag == "Muro"){
+                return true;
+            } 
+        } 
+        return false;           
+    }
+
+
+    void Movimento(Vector3 dir){
         Omino.transform.rotation = Quaternion.LookRotation(dir);
         rb.velocity = dir * velocita;  
     }
