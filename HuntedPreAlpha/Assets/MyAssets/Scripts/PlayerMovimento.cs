6cebf66f790a76f4f22fa2b05cc667ba183bfc8d
Rcurreli <ricurreli96@gmail.com>
2019-07-08 14:07:20 +0200

    8 luglio

--- HuntedPreAlpha/Assets/MyAssets/Scripts/PlayerMovimento.cs
+++ HuntedPreAlpha/Assets/MyAssets/Scripts/PlayerMovimento.cs
@@ -11,6 +11,20 @@ public class PlayerMovimento : MonoBehaviour
     private bool controllo;
     private Quaternion ominoDir;
     private Vector3 raggioDir;
+    private int asse; 
+    private Vector3 miaPosizione1;
+    private Vector3 miaPosizione2;
+    private RaycastHit chivedo1;
+    private RaycastHit chivedo2;
+    private bool vedoqualcosa1;
+    private bool vedoqualcosa2;
+    private Vector3 miaPosizione3;
+    private Vector3 miaPosizione4;
+    private RaycastHit chivedo3;
+    private RaycastHit chivedo4;
+    private bool vedoqualcosa3;
+    private bool vedoqualcosa4;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +33,7 @@ public class PlayerMovimento : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {    
         ominoDir = Omino.transform.rotation;
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
@@ -28,9 +42,12 @@ public class PlayerMovimento : MonoBehaviour
             }
             else if(ominoDir == Quaternion.LookRotation(- transform.forward)){
                 Movimento(transform.forward);
+                raggioDir = transform.forward;
             }
             else{
+                asse = 0;
                 raggioDir = transform.forward;
+                controllo = true;
             }
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow)) {
@@ -39,9 +56,12 @@ public class PlayerMovimento : MonoBehaviour
             }    
             else if(ominoDir == Quaternion.LookRotation(transform.forward)){
                 Movimento(- transform.forward);
+                raggioDir = - transform.forward;
             }
             else{
+                asse = 0;
                 raggioDir = - transform.forward;
+                controllo = true;
             }    
         }
 
@@ -51,9 +71,12 @@ public class PlayerMovimento : MonoBehaviour
             }  
             else if(ominoDir == Quaternion.LookRotation(transform.right)){
                 Movimento(- transform.right);
+                raggioDir = - transform.right;
             }
             else{
+                asse = 1;
                 raggioDir = - transform.right;
+                controllo = true;
             }      
         }
 
@@ -63,25 +86,78 @@ public class PlayerMovimento : MonoBehaviour
             }
             else if(ominoDir == Quaternion.LookRotation(- transform.right)){
                 Movimento(transform.right);
+                raggioDir = transform.right;
             }  
             else{
+                asse = 1;
                 raggioDir = transform.right;
+                controllo = true;
             }  
         }
 
-        Vector3 miaPosizione = transform.position;
-		miaPosizione.y += 0.5f;
+        //Raycast svolta
+        if(controllo){
+            if(raggioDir.x != 0 || raggioDir.z != 0){
+                miaPosizione3 = transform.position;
+                miaPosizione3.y += 0.5f;
+                miaPosizione4 = transform.position;
+                miaPosizione4.y += 0.5f;
+                if(asse == 0){
+                    miaPosizione3.x += 0.49f;
+                    miaPosizione4.x -= 0.49f;
+                }
+                else if(asse == 1){
+                    miaPosizione3.z += 0.49f;
+                    miaPosizione4.z -= 0.49f;
+                }
+                vedoqualcosa3 = Physics.Raycast(miaPosizione3, raggioDir, out chivedo3, 0.6f);
+                vedoqualcosa4 = Physics.Raycast(miaPosizione4, raggioDir, out chivedo4, 0.6f);
 
-		RaycastHit chivedo;
-		bool vedoqualcosa = Physics.Raycast(miaPosizione, raggioDir, out chivedo, 10f);
+                if ((!vedoqualcosa3 || chivedo3.transform.tag != "Muro")&&(!vedoqualcosa4 || chivedo4.transform.tag != "Muro")) {
+                    Movimento(raggioDir);
+                    controllo = false;
+                } 
+                Debug.DrawRay(miaPosizione3, raggioDir, Color.blue);
+                Debug.DrawRay(miaPosizione4, raggioDir, Color.blue);
+            }
+        }
 
-		if (vedoqualcosa && chivedo.transform.tag == "Muro") {
-            
-		} 
-        else {
-			GetComponent<Animator>().SetBool("tivedo", false);
-		}
-
+        //Raycast davanti
+        miaPosizione1 = transform.position;
+        miaPosizione1.y += 0.5f;
+        miaPosizione2 = transform.position;
+        miaPosizione2.y += 0.5f;
+        ominoDir = Omino.transform.rotation;
+        if(ominoDir == Quaternion.LookRotation(- transform.forward) || ominoDir == Quaternion.LookRotation(transform.forward)){
+            miaPosizione1.x += 0.49f;
+            miaPosizione2.x -= 0.49f;
+        }
+        else if(ominoDir == Quaternion.LookRotation(- transform.right) || ominoDir == Quaternion.LookRotation(transform.right)){
+            miaPosizione1.z += 0.49f;
+            miaPosizione2.z -= 0.49f;
+        }
+        vedoqualcosa1 = Physics.Raycast(miaPosizione1, Omino.transform.forward, out chivedo1, 0.49f);
+        vedoqualcosa2 = Physics.Raycast(miaPosizione2, Omino.transform.forward, out chivedo2, 0.49f);
+        if(vedoqualcosa1){
+            if(chivedo1.transform.tag == "Muro"){
+                rb.velocity*=0;
+                if(ominoDir == Quaternion.LookRotation(- transform.forward) || ominoDir == Quaternion.LookRotation(transform.forward)){
+                    int divisione = (int)(transform.position.z / 0.5f);
+                    transform.position = new Vector3(transform.position.x,transform.position.y,divisione * 0.5f);
+                }
+            } 
+        }
+        if(vedoqualcosa2){
+            if(chivedo2.transform.tag == "Muro"){
+                rb.velocity*=0;
+                if(ominoDir == Quaternion.LookRotation(- transform.right) || ominoDir == Quaternion.LookRotation(transform.right)){
+                    int divisione = (int)(transform.position.x / 0.5f);
+                    transform.position = new Vector3(divisione * 0.5f,transform.position.y,transform.position.z);
+                }
+            } 
+        }
+        Debug.DrawRay(miaPosizione1, Omino.transform.forward, Color.red);
+        Debug.DrawRay(miaPosizione2, Omino.transform.forward, Color.red);
     }
 
     private void Movimento(Vector3 dir){
